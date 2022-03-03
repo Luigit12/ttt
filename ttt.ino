4cea85d9684ab7181a007fc3c354d9143df7a6cb
Wouterr0 <ruiterwouter@gmail.com>
2022-03-03 14:38:22 +0100

    make them turn separately

--- ttt.ino
+++ ttt.ino
@@ -1,28 +1,37 @@
 #include <Stepper.h>
 
 const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
-
-Stepper stepper1(stepsPerRevolution, 19, 5, 18, 17);
-Stepper stepper2(stepsPerRevolution, 22, 3, 1, 21);
+                                  // IN1 IN3 IN2 IN4
+Stepper stepper1(stepsPerRevolution, 21, 18, 19, 5);
+Stepper stepper2(stepsPerRevolution, 17, 4, 16, 2);
 
 void setup() {
   // set the speed at 5 rpm
-  stepper1.setSpeed(5);
-  stepper2.setSpeed(5);
+  stepper1.setSpeed(15);
+  stepper2.setSpeed(15);
+  
   // initialize the serial port
   Serial.begin(115200);
 }
 
-void loop() {
-  // step one revolution in one direction:
-  Serial.println("clockwise");
-  stepper1.step(stepsPerRevolution);
-  stepper2.step(stepsPerRevolution);
-  delay(1000);
+void turnboth(float rev1, float rev2) { 
+  size_t count1 = 0
+  size_t count2 = 0;
+  int steps1 = rev1 * stepsPerRevolution;
+  int steps2 = rev2 * stepsPerRevolution;
 
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  stepper1.step(-stepsPerRevolution);
-  stepper2.step(-stepsPerRevolution);
-  delay(1000);
+  while ((count1 < steps1) && (count2 < steps2)) {
+    if (count1 < steps1) {
+      stepper1.step(1);
+      count1++;
+    }
+    if (count2 < steps2) {
+      stepper2.step(1);
+      count2++;
+    }
+  }
+}
+
+void loop() {
+  turnboth(1, 1);
 }
