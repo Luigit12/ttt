777368e30ce2b5c2ce82be10a16128dcf2b57c33
Wouterr0 <ruiterwouter@gmail.com>
2022-03-06 12:14:00 +0100

    Add `turn` serial command and code format

--- ttt.ino
+++ ttt.ino
@@ -3,25 +3,26 @@
 #define sign(x) ((0 < x) - (x < 0))
 
 const int speed = 15;
-const int stepsPerRevolution = 2048; // change this to fit the number of steps per revolution
+const int stepsPerRevolution =
+    2048;  // change this to fit the number of steps per revolution
 
-// ESP32 devkit pinout: https://www.mischianti.org/wp-content/uploads/2020/11/ESP32-DOIT-DEV-KIT-v1-pinout-mischianti.png
+// ESP32 devkit pinout:
+// https://www.mischianti.org/wp-content/uploads/2020/11/ESP32-DOIT-DEV-KIT-v1-pinout-mischianti.png
 // IN1 IN3 IN2 IN4
 Stepper stepper1(stepsPerRevolution, 13, 14, 12, 27);
 Stepper stepper2(stepsPerRevolution, 26, 33, 25, 32);
 Stepper stepper3(stepsPerRevolution, 15, 4, 2, 16);
 
-void setup()
-{
+void setup() {
   stepper1.setSpeed(speed);
   stepper2.setSpeed(speed);
   stepper3.setSpeed(speed);
 
   Serial.begin(9600);
+  Serial.println("ttt started");
 }
 
-void turnall(float rev1, float rev2, float rev3)
-{
+void turnall(float rev1, float rev2, float rev3) {
   Serial.print("turning: ");
   Serial.print(rev1);
   Serial.print(", ");
@@ -42,20 +43,16 @@ void turnall(float rev1, float rev2, float rev3)
 
   int max_target = max(max(target1, target2), target3);
 
-  for (int i = 0; i < max_target; i++)
-  {
-    if (steps1 * max_target < i * target1)
-    {
+  for (int i = 0; i < max_target; i++) {
+    if (steps1 * max_target < i * target1) {
       stepper1.step(dir1);
       steps1++;
     }
-    if (steps2 * max_target < i * target2)
-    {
+    if (steps2 * max_target < i * target2) {
       stepper2.step(dir2);
       steps2++;
     }
-    if (steps3 * max_target < i * target3)
-    {
+    if (steps3 * max_target < i * target3) {
       stepper3.step(dir3);
       steps3++;
     }
@@ -63,12 +60,16 @@ void turnall(float rev1, float rev2, float rev3)
   Serial.println("done");
 }
 
-void loop()
-{
-  if (Serial.available() && (Serial.readStringUntil('\n') == "go"))
-  {
-    Serial.println("received go");
-    turnall(2.f, -1.f, 7.f);
+void loop() {
+  if (Serial.available()) {
+    char command[64];
+    float rev1, rev2, rev3;
+    command[Serial.readBytesUntil('\n', command, sizeof(command))] = '\0';
+    Serial.print("got command: ");
+    Serial.println(command);
+    if (sscanf(command, "turn %f %f %f", &rev1, &rev2, &rev3) == 3) {
+      turnall(rev1, rev2, rev3);
+    }
   }
 
   // ttt positioning system
